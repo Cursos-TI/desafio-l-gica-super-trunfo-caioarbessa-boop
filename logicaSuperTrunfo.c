2b2c01466843f3f69305f908b8eccda7ca09859f
caioarbessa-boop <caioarbessa@gmail.com>
2025-10-31 16:39:30 -0300

    Update logicaSuperTrunfo.c

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,64 +1,85 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    
-    // Variáveis para a primeira carta:
-    char estado1[2], codigo1[4], nome_da_cidade1[50];
-    int populacao1;
-    float area_em_km1, densidade1;
+    // Variáveis das cartas
+    char nome1[50], nome2[50];
+    int populacao1, populacao2;
+    float area1, area2, pib1, pib2, densidade1, densidade2, pib_percapita1, pib_percapita2;
 
-    // Variáveis para a segunda carta:
-    char estado2[2], codigo2[4], nome_da_cidade2[50];
-    int populacao2;
-    float area_em_km2, densidade2;
+    // Cadastro da Carta 1
+    printf("=== Cadastro Carta 01 ===\n");
+    printf("Cidade: "); scanf(" %49[^\n]", nome1);
+    printf("População: "); scanf("%d", &populacao1);
+    printf("Área (km²): "); scanf("%f", &area1);
+    printf("PIB (milhões): "); scanf("%f", &pib1);
 
-    // Dados de entrada para a primeira carta
-    printf("Cadastro Carta 01\n");
-    printf("Digite o nome do Estado (ex: A-H): \n");
-    scanf("%1s", estado1);
-    printf("Digite o código da carta (ex: A01): \n");
-    scanf("%3s", codigo1);
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", nome_da_cidade1);
-    printf("Digite a população: \n");
-    scanf("%d", &populacao1);
-    printf("Digite a área em kilometros quadrados: \n");
-    scanf("%f", &area_em_km1);
+    // Cadastro da Carta 2
+    printf("\n=== Cadastro Carta 02 ===\n");
+    printf("Cidade: "); scanf(" %49[^\n]", nome2);
+    printf("População: "); scanf("%d", &populacao2);
+    printf("Área (km²): "); scanf("%f", &area2);
+    printf("PIB (milhões): "); scanf("%f", &pib2);
 
-    // Dados de entrada para a segunda carta
-    printf("\nCadastro Carta 02\n");
-    printf("Digite o nome do Estado (ex: A-H): \n");
-    scanf("%1s", estado2);
-    printf("Digite o código da carta (ex: A01): \n");
-    scanf("%3s", codigo2);
-    printf("Digite o nome da cidade: \n");
-    scanf("%s", nome_da_cidade2);
-    printf("Digite a população: \n");
-    scanf("%d", &populacao2);
-    printf("Digite a área em kilometros quadrados: \n");
-    scanf("%f", &area_em_km2);
+    // Cálculos
+    densidade1 = populacao1 / area1;
+    densidade2 = populacao2 / area2;
+    pib_percapita1 = (pib1 * 1e6) / populacao1;
+    pib_percapita2 = (pib2 * 1e6) / populacao2;
 
-    // Calcular densidade populacional
-    densidade1 = populacao1 / area_em_km1;
-    densidade2 = populacao2 / area_em_km2;
+    // Menu dinâmico
+    int opcao1, opcao2;
+    printf("\n=== MENU DE ATRIBUTOS ===\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - PIB per capita\n");
+    printf("5 - Densidade populacional (menor vence)\n");
+    printf("Escolha o primeiro atributo: ");
+    scanf("%d", &opcao1);
+    printf("Escolha o segundo atributo (para desempate): ");
+    scanf("%d", &opcao2);
 
-    printf("\n--- Comparação de Densidade Populacional ---\n");
-    printf("%s: %.2f hab/km²\n", nome_da_cidade1, densidade1);
-    printf("%s: %.2f hab/km²\n", nome_da_cidade2, densidade2);
+    // Função para pegar o valor de cada atributo
+    float valor1_carta1, valor1_carta2, valor2_carta1, valor2_carta2;
 
-    // Determinar vencedor (maior densidade perde)
-    if(densidade1 > densidade2) {
-        printf("A cidade vencedora é: %s\n", nome_da_cidade2);
-    } else if(densidade2 > densidade1) {
-        printf("A cidade vencedora é: %s\n", nome_da_cidade1);
-    } else {
-        printf("Empate! As duas cidades têm a mesma densidade.\n");
-    }
+    // Atribuição dinâmica (1º atributo)
+    valor1_carta1 = (opcao1 == 1) ? populacao1 :
+                    (opcao1 == 2) ? area1 :
+                    (opcao1 == 3) ? pib1 :
+                    (opcao1 == 4) ? pib_percapita1 :
+                    (1 / densidade1);  // inverso para densidade (menor vence)
+
+    valor1_carta2 = (opcao1 == 1) ? populacao2 :
+                    (opcao1 == 2) ? area2 :
+                    (opcao1 == 3) ? pib2 :
+                    (opcao1 == 4) ? pib_percapita2 :
+                    (1 / densidade2);
+
+    // Atribuição dinâmica (2º atributo)
+    valor2_carta1 = (opcao2 == 1) ? populacao1 :
+                    (opcao2 == 2) ? area1 :
+                    (opcao2 == 3) ? pib1 :
+                    (opcao2 == 4) ? pib_percapita1 :
+                    (1 / densidade1);
+
+    valor2_carta2 = (opcao2 == 1) ? populacao2 :
+                    (opcao2 == 2) ? area2 :
+                    (opcao2 == 3) ? pib2 :
+                    (opcao2 == 4) ? pib_percapita2 :
+                    (1 / densidade2);
+
+    // Comparação dupla (aninhada e com operador ternário)
+    char *vencedor =
+        (valor1_carta1 > valor1_carta2) ? nome1 :
+        (valor1_carta2 > valor1_carta1) ? nome2 :
+        (valor2_carta1 > valor2_carta2) ? nome1 :
+        (valor2_carta2 > valor2_carta1) ? nome2 : "Empate";
+
+    printf("\n=== RESULTADO ===\n");
+    if (vencedor == "Empate")
+        printf("Empate entre %s e %s!\n", nome1, nome2);
+    else
+        printf("A cidade vencedora é: %s\n", vencedor);
 
     return 0;
 }
